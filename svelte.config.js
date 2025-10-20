1c6f83ab4829df6ccc211639a28fa88a824ba928
Kieran <kieranpisani2007@gmail.com>
2025-10-20 21:36:20 +0200

    Alpha 0.5.6

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,8 @@
 import adapter from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
 const config = {
   kit: {
     adapter: adapter(),
   },
-  preprocess: preprocess(),
 };
 
 export default config;
